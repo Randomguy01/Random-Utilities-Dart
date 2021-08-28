70cbdaff34764e3b7f05e0e042677f9836b8a393
Randomguy01 <ianhwhite2001@gmail.com>
2021-08-27 20:44:46 -0400

    Added example usage of replaceAt() function
    Signed-off-by: Randomguy01 <ianhwhite2001@gmail.com>

--- example/string_utilities/string_extensions.dart
+++ example/string_utilities/string_extensions.dart
@@ -1,5 +1,14 @@
-
+import 'package:random_utilities_dart/string_utilities.dart';
 
 void main() {
+  //replaceAt() ----------------------------------------------------------------
+  //Replace character with another character
+  String misspelled = "rello World";
+  String fixed = misspelled.replaceAt(0, "H");
+  print("Before: $misspelled, After: $fixed");
 
+  //Replace character with another word
+  misspelled = "Hello  ";
+  fixed = misspelled.replaceAt(misspelled.length - 1, "World");
+  print("Before: $misspelled, After: $fixed");
 }
